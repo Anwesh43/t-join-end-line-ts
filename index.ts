70638d9d61287849137c38ea4ec840adcf0601a6
Anwesh43 <anweshthecool0@gmail.com>
2020-12-02 00:05:14 +0530

    adding TJoinEndLine class

--- index.ts
+++ index.ts
@@ -184,3 +184,26 @@ class TJELNode {
         return this 
     }
 }
+
+class TJoinEndLine {
+
+    curr : TJELNode = new TJELNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+                cb()
+            })
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
